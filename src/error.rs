a981303a49f035fd7cdd823179edf03c43c2f087
Akash <akashgurava@outlook.com>
2022-08-03 20:25:20 +0530

    remove serialization

--- src/error.rs
+++ src/error.rs
@@ -2,6 +2,4 @@
 pub enum JunoRustError {
     #[error("Could not read file.")]
     FileReadError(#[from] std::io::Error),
-    #[error("Could not serialize JSON file.")]
-    SerializeError(#[from] serde_json::Error),
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,5 @@
-use std::fs::read;
-
 pub mod error;
-pub mod types;
 
 pub use error::JunoRustError;
-pub use types::{Geometry, Properties, SfCityLot};
 
-fn read_mid(path: String) -> Result<Vec<SfCityLot>, JunoRustError> {
-    let data = read(path)?;
-    let data: Vec<SfCityLot> = serde_json::from_slice(&data)?;
-    Ok(data)
-}
-
-uniffi_macros::include_scaffolding!("junorust");
+uniffi_macros::include_scaffolding!("JunoRust");
